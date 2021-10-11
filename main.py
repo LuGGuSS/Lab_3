eecf3ce97b681a50125e337babad81421b37c73c
Yurii <90028497+LuGGuSS@users.noreply.github.com>
2021-10-11 13:07:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+i = 0
+for i in range (0,4):
+    x = int(input("x = "))
+    print("x =", f"{x:>3}")
+    i += 1
+input()
+
+i = 0
+for i in range (0,2):
+    x = float(input("x = "))
+    print("x =", f"{x:>9}")
+input()
+
+i = 0
+for i in range (0,2):
+    x = float(input("x = "))
+    print("x =", f"{x:5.2f}")
+input()
+
+s = input("Input string: ")
+print(s[0:3])
+
+b = False
+print(str(b))
+input()
